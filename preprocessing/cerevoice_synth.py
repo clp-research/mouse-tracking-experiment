c044598281a7b99c8ebe323e5ce25d1027b73dd9
Simeon Schüz <simeon.schuez@uni-bielefeld.de>
2018-10-18 12:17:23 +0200

    added initial files

---
+++ preprocessing/cerevoice_synth.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+# Copyright (c) 2011-2016 CereProc Ltd.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+# make shure cerevoice_eng and Voice folders are in the same directory!
+
+import os
+import sys
+
+dir = os.path.dirname(os.path.realpath(__file__))
+engdir = os.path.join(dir, 'cerevoice_eng')
+sys.path.append(engdir)
+
+import cerevoice_eng
+
+def synth(input_str, filename="output.wav", outdir=None):
+
+    if not outdir:
+        cwd = os.getcwd()
+        outdir = cwd
+    licensefile = os.path.dirname(os.path.abspath(__file__))+"/Voice/heather.lic"
+    voicefile =os.path.dirname(os.path.abspath(__file__))+ "/Voice/cerevoice_heather_4.0.0_48k.voice"
+    ondisk = False
+
+    # Create an engine
+    engine = cerevoice_eng.CPRCEN_engine_new()
+
+    # Set the loading mode - all data to RAM or with audio and indexes on disk
+    loadmode = cerevoice_eng.CPRC_VOICE_LOAD
+    if ondisk:
+        loadmode = cerevoice_eng.CPRC_VOICE_LOAD_EMB
+
+    # Load the voice
+    ret = cerevoice_eng.CPRCEN_engine_load_voice(engine, licensefile, "", voicefile, loadmode)
+    if not ret:
+        sys.stderr.write("ERROR: could not load the voice, check license integrity\n")
+        sys.exit(1)
+
+    # Get some information about the first loaded voice (index 0)
+    name = cerevoice_eng.CPRCEN_engine_get_voice_info(engine, 0, "VOICE_NAME");
+    srate = cerevoice_eng.CPRCEN_engine_get_voice_info(engine, 0, "SAMPLE_RATE");
+    #sys.stderr.write("INFO: voice name is '%s', sample rate '%s'\n" % (name, srate))
+
+    wavout = os.path.join(outdir) + "/{name}".format(name=filename)
+    cerevoice_eng.CPRCEN_engine_speak_to_file(engine, input_str, wavout)
+    #sys.stderr.write("INFO: wrote wav file '%s'\n" % wavout)
+
+    # Clean up
+    cerevoice_eng.CPRCEN_engine_delete(engine)
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("string")
+    parser.add_argument("filename")
+    parser.add_argument("directory")
+
+    args = parser.parse_args()
+
+    synth(args.string, args.filename, args.directory)
---
+++ preprocessing/create_json.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import json
+import os
+import datetime
+import argparse
+from cerevoice_synth import synth
+from shutil import copyfile
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n", help='number of json files', default=5, type=int)
+parser.add_argument("--s", help='number of entries in each json file', default=10, type=int)
+parser.add_argument("--o", help='path for output', default=os.getcwd() )
+parser.add_argument("--p", help='path for json input', default=os.getcwd() )
+parser.add_argument("--i", help='path to image corpus', default="/media/dsgserve1/Corpora/External/ImageCorpora/MSCOCO/train2014")
+args = parser.parse_args()
+
+n = args.n
+sample_size = args.s
+in_dir = os.path.abspath(args.p)
+cwd = os.path.abspath(args.o)
+image_dir = os.path.abspath(args.i)
+timestamp = datetime.datetime.now().isoformat()
+
+def img_filename(image_id):
+    """ create img filename from id """
+    id = str(image_id)
+    n = 12-len(id)
+    img = (n*"0")+id
+    return ("COCO_train2014_"+img+".jpg")
+
+if __name__ == '__main__':
+
+    # switch to output directory
+    os.chdir(cwd)
+
+    # make sure appropriate directories exist
+    folders = ["/images", "/audio", "/json"]
+    for folder in folders:
+        if not os.path.exists(cwd+folder):
+            os.makedirs(cwd+folder)
+            print("created directory: "+cwd+folder)
+
+    # import json files as data frames
+    bbdf = pd.read_json(in_dir+"/"+'mscoco_bbdf.json.gz',compression='gzip', orient='split')
+    refdf = pd.read_json(in_dir+"/"+'refcoco_refdf.json.gz',compression='gzip', orient='split')
+    # extract unique image_ids from refdf
+    image_ids = refdf['image_id'].unique()
+
+    for m in range(n):
+
+        # get 10 random image_ids
+        sample = np.random.choice(image_ids, sample_size)
+
+        #create new DataFrame with columns from refdf and bbdf
+        clmns = set(list(bbdf.columns)+list(refdf.columns)+["image_filename", "audio_filename"])
+        resultdf = pd.DataFrame(columns=clmns)
+
+        #iterate through image_ids in sample,
+        for i in sample:
+            # pick random refexp and find respective entries in bbdf, then merge
+            rfdf_img = refdf[refdf["image_id"]==i].sample(1)
+            rslt = pd.merge(rfdf_img, bbdf)
+
+            # copy the corresponding image into image output folder
+            image_filename = img_filename(i)
+            img_source = image_dir + "/" + image_filename
+            img_destination = cwd+"/images/" + image_filename
+            copyfile(img_source, img_destination)
+
+            # create audio file with refexp and store in audio output folder
+            refexp = rslt["refexp"].values[0]
+            rexid = rslt["rex_id"].values[0]
+            audio_filename ="{img_id}-{rex_id}.wav".format(img_id=i,rex_id=rexid)
+            synth("Please click on the "+str(refexp), filename=audio_filename, outdir="audio")
+
+            # add image and audio file names and append to results
+            rslt["image_filename"]= image_filename
+            rslt["audio_filename"]= audio_filename
+            resultdf = resultdf.append(rslt, ignore_index=True)
+
+        #return results as json file and store in json output folder
+        jsondata = json.loads(resultdf.to_json(orient='index'))
+        jsondata["used"]=False
+        filename = 'json/{time}-{number}.json'.format(time=timestamp, number=m)
+        with open(filename, 'w') as outfile:
+            json.dump(jsondata, outfile, sort_keys=True, indent=1)
